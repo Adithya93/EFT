0f3c7aa051ff2302736391950e02ef7ef41c5752
eae15 <eae15@duke.edu>
2017-03-30 12:34:07 -0400

    Add files via upload

--- Project/lookUpTable/comparator.vhd
+++ Project/lookUpTable/comparator.vhd
@@ -13,5 +13,47 @@ end comparator;
 
 architecture comparator_arch of comparator is
 begin
-
+	process(da, addr1, compResult, compDone)
+	begin
+		comp1 <= '0';
+		greater <= '0'
+		equal <= '0'
+		less <= '0'
+		if (da > addr1) then
+			greater <= '1';
+			compResult[0] <= '0';
+		elsif (da = addr1) then
+			equal <= '1';
+			compResult[0] <= '1';
+		elsif (da < addr1) then
+			less <= '1';
+			compResult[0] <= '0';
+		end if;
+		comp1 <= '1';
+		if (comp1 and comp2) then
+			compDone <= '1';
+		end if;		
+	end process;
+	
+	process(da, addr2. compResult, compDone)
+	begin
+		comp2 <= '0';
+		greater <= '0'
+		equal <= '0'
+		less <= '0'
+		if (da > addr2) then
+			greater <= '1';
+			compResult[1] <= '0';
+		elsif (da = addr2) then
+			equal <= '1';
+			compResult[1] <= '1';
+		elsif (da < addr2) then
+			less <= '1';
+			compResult[1] <= '0';
+		end if;
+		comp2 <= '1';
+		if (comp1 and comp2) then
+			compDone <= '1';
+		end if;
+	end process;
 end comparator_arch;
