7cdb193505c2dcaba495bcad2e5953137f5f0097
Adithya <ra102@duke.edu>
2017-04-06 03:16:23 -0400

    Fix bug in LRU & Comparator, tests to verify their correctness

--- Project/lookUpTable/lookUpTable.vhd
+++ Project/lookUpTable/lookUpTable.vhd
@@ -12,17 +12,12 @@ entity lookUpTable is
 		destPort_sig: 								out std_logic_vector(9 downto 0);
 		isHit_sig:									out std_logic;
 		lookNowOut_sig:							out std_logic;
-		glob_timer_sig:							out std_logic_vector(34 downto 0);
+		glob_timer_sig:							out std_logic_vector(9 downto 0);
 		identf_sig:									buffer std_logic_vector(1 downto 0);
 		da_sig:										buffer std_logic_vector(47 downto 0);
-		addr1_sig:									buffer std_logic_vector(47 downto 0);
-		addr2_sig:									buffer std_logic_vector(47 downto 0);
---		state_num:									buffer std_logic_vector(2 downto 0);
-		compDone_sig:								buffer std_logic;
-		compareLook_sig:							buffer std_logic);
---		comp_state_num_sig:						out std_logic_vector(1 downto 0);
---		lru_out_sig:								out std_logic;
---		h_state_num_sig:							out std_logic_vector(2 downto 0));
+		addr1_sig:										buffer std_logic_vector(47 downto 0);
+		addr2_sig:										buffer std_logic_vector(47 downto 0);
+		state_num:										buffer std_logic_vector(2 downto 0));
 end lookUpTable;
 
 architecture mult_subsystem of lookUpTable is
@@ -35,25 +30,24 @@ architecture mult_subsystem of lookUpTable is
 			srcPort:										 in std_logic_vector(9 downto 0);
 			notValid: 									 in std_logic_vector(5 downto 0);
 			destPort:									out std_logic_vector(9 downto 0);
-			cAddrOne, cAddrTwo:		out std_logic_vector(47 downto 0);
+			cAddrOne, cAddrTwo:		buffer std_logic_vector(47 downto 0);
 			setRegAge:									out std_logic_vector(5 downto 0);
-			compareLook, resetAgeing, isHit:						out std_logic;
+			compareLook, resetAgeing, isHit:		out std_logic;
+			hashVal:								buffer std_logic_vector(4 downto 0);
+			crc_reset:							buffer std_logic;
 			isDone:										out std_logic);
---			lru_out:										out std_logic;
---			h_state_num:								out std_logic_vector(2 downto 0));
 	end component;
 
 	component comparator
 		port(
-			clk:		in std_logic;
-			reset:	in std_logic;
+			clk_sig:		in std_logic;
+			reset_sig:	in std_logic;
+			da:		in std_logic_vector(47 downto 0);
 			addr1:	in std_logic_vector(47 downto 0);
 			addr2:	in std_logic_vector(47 downto 0);
-			da:		in std_logic_vector(47 downto 0);
-			compStart: in std_logic;
 			compResult:	out std_logic_vector(1 downto 0);
-			compDone:	out std_logic);
---			comp_state_num:	out std_logic_vector(1 downto 0));
+			compDone:	out std_logic;
+			compStart: in std_logic);
 		end component;
 		
 
@@ -64,26 +58,21 @@ architecture mult_subsystem of lookUpTable is
 			set_reg_age: in std_logic_vector(5 downto 0);
 			age_out_now: out std_logic;
 			age_out_reg: out std_logic_vector(5 downto 0);
-			glob_timer:	 out std_logic_vector(34 downto 0));
+			glob_timer:	 out std_logic_vector(9 downto 0));
 		end component;
 		
-		--signal compDone_sig, resetAgeing_sig, ageOut_sig: 		std_logic;
-		signal resetAgeing_sig, ageOut_sig: 		std_logic;
+		signal compDone_sig, resetAgeing_sig, ageOut_sig: 		std_logic;
 		--signal addr1_sig, addr2_sig: 									std_logic_vector(47 downto 0);
 		--signal identf_sig:												std_logic_vector(1 downto 0);
 		signal notValid_sig: 											std_logic_vector(5 downto 0);
 		signal setRegAge_sig:											std_logic_vector(5 downto 0);
-		--signal compareLook_sig:											std_logic;
+		signal compareLook_sig:											std_logic;
 		--signal da_sig:														std_logic_vector(47 downto 0);
-		
+		signal destPort_latch:											std_logic_vector(9 downto 0);
 		signal isDone:														std_logic;
+		signal isHit_latch:												std_logic;
+		signal keepHit:													std_logic;
 		--signal hashVal_sig:												std_logic_vector(4 downto 0);
-		
---		signal	comp_state_num_sig:						out std_logic_vector(1 downto 0);
---		signal	lru_out_sig:								out std_logic;
---		signal	h_state_num_sig:							out std_logic_vector(2 downto 0);
-
-		signal state_num:	std_logic_vector(2 downto 0);
 		
 		type state_type is
 			(P, Q, R, S, T, U, V);
@@ -102,27 +91,24 @@ architecture mult_subsystem of lookUpTable is
 										identf => identf_sig,
 										srcPort => srcPort_sig,
 										notValid => notValid_sig,
-										destPort => destPort_sig,
+										destPort => destPort_latch,
 										--destAddOut => destAddrOut_sig,
 										cAddrOne => addr1_sig,
 										cAddrTwo => addr2_sig,
 										setRegAge => setRegAge_sig,
 										compareLook => compareLook_sig,
 										resetAgeing => resetAgeing_sig,
-										isHit => isHit_sig,
-										isDone => isDone);--,
---										lru_out => lru_out_sig,
---										h_state_num => h_state_num_sig);
+										isHit => isHit_latch,
+										isDone => isDone);
 										
-		U1: comparator port map(clk => clk_sig,
-										reset => reset_sig,
+		U1: comparator port map(clk_sig => clk_sig,
+										reset_sig => reset_sig,
+										da =>		da_sig, -- needs to get assigned to DA & SA based on cycle, through FSM
 										addr1 => addr1_sig,
 										addr2 => addr2_sig,
-										da =>		da_sig, -- needs to get assigned to DA & SA based on cycle, through FSM
-										compStart =>	compareLook_sig,
 										compResult => identf_sig,
-										compDone => compDone_sig);--,
---										comp_state_num =>	comp_state_num_sig);
+										compDone => compDone_sig,
+										compStart =>	compareLook_sig);
 										
 		U2: agingModule port map(clk => clk_sig,
 										 global_reset => reset_sig,
@@ -139,22 +125,31 @@ architecture mult_subsystem of lookUpTable is
 			end if;
 		end process;
 		
-		process(state_reg, lookNowIn_sig, isDone, destAddrIn_sig, srcAddr_sig)
+		process(state_reg, lookNowIn_sig, isDone, destAddrIn_sig, srcAddr_sig, isHit_latch, destPort_latch, keepHit)
 		begin
-		
+		isHit_sig <= keepHit;
 			case state_reg is
 				when P => 
 					if(lookNowIn_sig = '1') then
 						state_next <= Q;
 						da_sig <= destAddrIn_sig;
+						isHit_sig <= '0';
 					else
 						state_next <= P;
+						isHit_sig <= '0';
 					end if;
 				when Q =>
 					if(isDone = '1') then
 						state_next <= R;
+						destPort_sig <= destPort_latch;
+						if(isHit_latch = '1') then
+							keepHit <= '1';
+						else
+							keepHit <= '0';
+						end if;
 					else
 						state_next <= Q;
+						isHit_sig <='0';
 					end if;
 				when R =>
 						state_next <= S;
@@ -166,7 +161,9 @@ architecture mult_subsystem of lookUpTable is
 				when U => 
 					state_next <= V;
 				when V =>
-					state_next <= P;					
+					state_next <= P;
+					destPort_sig <= "0000000000";
+					isHit_sig <= '0';
 				end case;
 			end process;	
 
